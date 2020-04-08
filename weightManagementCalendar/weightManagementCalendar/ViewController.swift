8f7dd7732b4d7b6ef135cf5e09fa48f0288cbd3a
yukitotsuka <61972160+yukitotsuka@users.noreply.github.com>
2020-04-08 20:39:43 +0900

    Add files via upload
    just create pfoject

---
+++ weightManagementCalendar/weightManagementCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weightManagementCalendar
+//
+//  Created by 戸塚悠喜 on 2020/04/08.
+//  Copyright © 2020 Yuki Totsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
